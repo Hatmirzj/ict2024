8576e1b2bf3dea9157d537662c125a554ab95d26
Vangala Aruna <arunajava567@gmail.com>
2024-11-16 14:06:56 +0530

    Add files via upload

---
+++ SpringBootSecurity/spring-security-project-1/src/test/java/com/adp/springsecurityproject1/SpringSecurityProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adp.springsecurityproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
